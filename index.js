0bd939617924b6a12f21af08d79aa21e1d5501dd
urahiroshi <asj.hiroshi@gmail.com>
2019-02-10 00:07:06 +0900

    output response

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const http = require('http');
 const httpProxy = require('http-proxy');
 const proxy = httpProxy.createProxyServer({});
 
+proxy.on('proxyReq', (proxyReq) => {
+  proxyReq.setHeader('host', 'httpbin.org');
+});
+
+proxy.on('proxyRes', (proxyRes) => {
+  let rawBody = new Buffer('');
+  proxyRes.on('data', (data) => {
+    rawBody = Buffer.concat([rawBody, data]);
+  });
+  proxyRes.on('end', () => {
+    const body = rawBody.toString();
+    console.log({
+      headers: proxyRes.headers,
+      statusCode: proxyRes.statusCode,
+      body,
+    });
+  });
+})
+
 const server = http.createServer((req, res) => {
   proxy.web(req, res, { target: 'http://httpbin.org' });
 });
