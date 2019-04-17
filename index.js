4ff47b80fb52708ef91f23036a5d8c1d953600ca
urahiroshi <urahiroshi@mercari.com>
2019-04-17 14:18:35 +0900

    Runnning method move to run.js

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const MODE = process.env.MODE || 'record';
+const run = require('./lib/run');
 
-let run;
-if (MODE === 'record') {
-  run = require('./lib/record');
-} else if (MODE === 'replay') {
-  run = require('./lib/replay');
-}
 module.exports = { run };
